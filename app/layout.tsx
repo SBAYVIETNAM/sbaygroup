58eee984409a4b3a657b1780e547337465481d76
ChungLeba <lebachungtdh@gmail.com>
2023-07-28 09:19:19 +0700

    bo sung google analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="vi" data-theme="light">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-4FYG8CZ5SC" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-4FYG8CZ5SC');
+        `}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   )
