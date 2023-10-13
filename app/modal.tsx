a16a76e2021a6ba59535d6b7cfb966ba6f863e67
ChungLeba <lebachungtdh@gmail.com>
2023-10-13 14:51:42 +0700

    update btn cskh

--- app/modal.tsx
+++ app/modal.tsx
@@ -39,10 +39,10 @@ const Popup = ({handleClose}: any) => {
         
                  <div className="flex p-2 w-full flex-row items-center justify-between">
                    <div>
-                     <h2 className='text-red-600 text-lg font-bold'>Đăng ký hợp tác kinh doanh</h2>
+                     {/* <h2 className='text-red-600 text-lg font-bold'>Đăng ký hợp tác kinh doanh</h2> */}
                    </div>
                    <div>
-                     <button type="button" onClick={handleClose} className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Close</button>
+                     <button type="button" onClick={handleClose} className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Đóng</button>
                    </div>
                  </div>            
            </div>
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Footer from './_components/footer';
 import Link from 'next/link';
 import Popup from "./modal";
 
-export default function Home() {  
+export default function Home() {
   const [showModal, setShowModal] = useState(false);
   useEffect(() => {
     setTimeout(() => {
@@ -37,73 +37,75 @@ export default function Home() {
             <div className="video-docker absolute top-0 left-0 w-full h-screen overflow-hidden z-0">
               <video className="min-w-full min-h-full absolute object-cover" src="/img/bg-video.mp4" autoPlay muted loop></video>
             </div>
-            <div className="max-w-md z-10">
-              <h1 className="text-5xl font-bold text-cyan-50 mb-0">Sbay Việt Nam</h1>
-              <p className="py-3 text-cyan-50">Kiến tạo sự nghiệp.</p>
-            </div>
-            <div className='grid grid-cols-2 gap-3 z-10'>
+            <div className='z-10'>
+              <div className="max-w-md z-10">
+                <h1 className="text-5xl font-bold text-cyan-50 mb-0">Sbay Việt Nam</h1>
+                <p className="py-3 text-cyan-50">Kiến tạo sự nghiệp.</p>
+              </div>
+              <div className='grid grid-cols-2 gap-2 w-72 mx-auto'>
 
-              <Link href={'https://flight.sbaygroup.com'} target="_blank">
-                <div className='flex flex-col hover:animate-pulse hover:outline outline-1 rounded-xl outline-indigo-200 p-2'>
-                  <div>
-                    <div className="avatar">
-                      <div className="w-16 mask mask-squircle">
-                        <Image width={256} height={256} src="/img/air.png" alt='ve-may-bay' style={{ 'objectFit': 'contain' }} />
+                <Link href={'https://flight.sbaygroup.com'} target="_blank">
+                  <div className='flex flex-col hover:animate-pulse hover:outline outline-1 rounded-xl outline-indigo-200 p-2'>
+                    <div>
+                      <div className="avatar">
+                        <div className="w-16 mask mask-squircle">
+                          <Image width={256} height={256} src="/img/air.png" alt='ve-may-bay' style={{ 'objectFit': 'contain' }} />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div>
-                    <h2 className='  font-bold text-cyan-50'>Vé máy bay</h2>
-                  </div>
-                </div>
-              </Link>
-              <Link href={'https://www.sbayhoteldanang.com/'} target="_blank">
-                <div className='flex flex-col hover:animate-pulse hover:outline outline-1 rounded-xl outline-indigo-200 p-2'>
-                  <div>
-                    <div className="avatar">
-                      <div className="w-16 mask mask-squircle">
-                        <Image width={256} height={256} alt='Phòng khách sạn Đà Nẵng' src="/img/hotel.png" />
-                      </div>
+                    <div>
+                      <h2 className='  font-bold text-cyan-50'>Vé máy bay</h2>
                     </div>
                   </div>
-                  <div>
-                    <h2 className='  font-bold text-cyan-50'>Khách sạn</h2>
-                  </div>
-                </div>
-              </Link>
-
-              <Link href={'https://datxe.sbayvietnam.com/'} target="_blank">
-                <div className='flex flex-col hover:animate-pulse hover:outline outline-1 rounded-xl outline-indigo-200 p-2'>
-                  <div>
-                    <div className="avatar">
-                      <div className="w-16 mask mask-squircle">
-                        <Image width={256} height={256} alt='xe cho thuê' src="/img/thue-xe.png" />
+                </Link>
+                <Link href={'https://www.sbayhoteldanang.com/'} target="_blank">
+                  <div className='flex flex-col hover:animate-pulse hover:outline outline-1 rounded-xl outline-indigo-200 p-2'>
+                    <div>
+                      <div className="avatar">
+                        <div className="w-16 mask mask-squircle">
+                          <Image width={256} height={256} alt='Phòng khách sạn Đà Nẵng' src="/img/hotel.png" />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div>
-                    <h2 className='  font-bold text-cyan-50'>Xe cho thuê</h2>
-
-                  </div>
-                </div>
-              </Link>
-              <Link href={'https://datve.sbayvietnam.com/'} target="_blank">
-                <div className='flex flex-col hover:animate-pulse hover:outline outline-1 rounded-xl outline-indigo-200 p-2'>
-                  <div>
-                    <div className="avatar">
-                      <div className="w-16 mask mask-squircle">
-                        <Image width={256} height={256} alt='vé Bà Nà' src="/img/ticket.png" />
-                      </div>
+                    <div>
+                      <h2 className='  font-bold text-cyan-50'>Khách sạn</h2>
                     </div>
                   </div>
-                  <div>
-                    <h2 className='  font-bold text-cyan-50'>Vé Bà Nà</h2>
+                </Link>
 
+                <Link href={'https://datxe.sbayvietnam.com/'} target="_blank">
+                  <div className='flex flex-col hover:animate-pulse hover:outline outline-1 rounded-xl outline-indigo-200 p-2'>
+                    <div>
+                      <div className="avatar">
+                        <div className="w-16 mask mask-squircle">
+                          <Image width={256} height={256} alt='xe cho thuê' src="/img/thue-xe.png" />
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <h2 className='  font-bold text-cyan-50'>Xe cho thuê</h2>
+
+                    </div>
                   </div>
-                </div>
-              </Link>
+                </Link>
+                <Link href={'https://datve.sbayvietnam.com/'} target="_blank">
+                  <div className='flex flex-col hover:animate-pulse hover:outline outline-1 rounded-xl outline-indigo-200 p-2'>
+                    <div>
+                      <div className="avatar">
+                        <div className="w-16 mask mask-squircle">
+                          <Image width={256} height={256} alt='vé Bà Nà' src="/img/ticket.png" />
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <h2 className='  font-bold text-cyan-50'>Vé Bà Nà</h2>
 
-              {/* <div className='flex flex-col hover:animate-pulse hover:outline outline-1 rounded-xl outline-indigo-200 p-2'>
+                    </div>
+                  </div>
+                </Link>
+
+
+                {/* <div className='flex flex-col hover:animate-pulse hover:outline outline-1 rounded-xl outline-indigo-200 p-2'>
               <div>
                 <div className="avatar">
                   <div className="w-16 mask mask-squircle">
@@ -129,68 +131,70 @@ export default function Home() {
 
               </div>
             </div> */}
+              </div>
+              <a href='https://cskh.sbayvietnam.com' className="btn btn-wide mx-auto my-5">Chăm sóc khách hàng</a>
+              <div className="max-w-md z-10">
+              <p className="text-cyan-50 mt-2">Ứng dụng Sbay hiện có sẵn trên cửa hàng ứng dụng, hãy cài đặt để trải ngiệm</p>
             </div>
-            <div className="max-w-md z-10">
-              {/*           <p className="py-3 text-cyan-50 mt-5">Sbay Việt Nam hiện đang sở hữu hệ thống chuỗi hơn 500 văn phòng trực thuộc đồng bộ thương hiệu, hơn 8.000 đại lý, phòng vé liên kết phủ khắp 64 tỉnh thành tại Việt Nam</p>
- */}          <p className="text-cyan-50 mt-2">Ứng dụng Sbay hiện có sẵn trên cửa hàng ứng dụng, hãy cài đặt để trải ngiệm</p>
-            </div>
-            <div className=" grid grid-cols-2 max-w-md z-10">
+            <div className=" grid grid-cols-2 max-w-md z-10 mx-auto my-5">
 
               <Link href={'https://apps.apple.com/vn/app/sbaygroup/id64462563480?l=vi'}>
-                <Image src={'/img/ios.png'} width={120} height={30} alt={'Sbay iso'} className=' rounded-sm mr-2'></Image>
+                <Image src={'/img/ios.png'} width={120} height={30} alt={'Sbay iso'} className=' rounded-sm mr-2 mx-auto'></Image>
               </Link>
               <Link href={'https://play.google.com/store/apps/details?id=com.sbgroup.booking_android&hl=vi-VN'}>
-                <Image src={'/img/gg-play.png'} width={120} height={30} alt={'Sbay iso'} className=' rounded-sm ml-2'></Image>
+                <Image src={'/img/gg-play.png'} width={120} height={30} alt={'Sbay iso'} className=' rounded-sm ml-2 mx-auto'></Image>
               </Link>
 
             </div>
 
-            <div className='grid grid-cols-2 gap-5 z-10'>
-              <Link href={'https://sbay.com.vn/gioi-thieu'} target="_blank" className='text-white text-sm flex flex-row justify-end'>
-                Giới thiệu về Sbay<Icon.ExternalLink className='w-4 h-4 ml-2' />
-              </Link>
-              <Link href={'https://sbay.com.vn/tin-tuc/chinh-sach-hop-tac-dai-ly-doi-tac/chuong-trinh-hop-tac-dai-ly-ve-may-bay-sbay-viet-nam-n4136'} target="_blank" className='text-white text-sm flex flex-row justify-start'>
-                Chương trinh hợp tác <Icon.ExternalLink className='w-4 h-4 ml-2' />
-              </Link>
-              <Link href={'https://flight.sbaygroup.com/?dang-nhap=1'} target="_blank" className='text-white text-sm flex flex-row justify-end'>
-                Trang Thành viên <Icon.ExternalLink className='w-4 h-4 ml-2' />
-              </Link>
-              <Link href={'https://work.sbayvietnam.com'} target="_blank" className='text-white text-sm flex flex-row justify-start'>
-                Trang WorkPlace <Icon.ExternalLink className='w-4 h-4 ml-2' />
-              </Link>
-              <Link href={'https://meet.google.com/coo-epfp-fnz'} target="_blank" className='text-white text-sm flex flex-row justify-end'>
-                Meet hàng ngày <Icon.ExternalLink className='w-4 h-4 ml-2' />
-              </Link>
-              <Link href={'https://docs.google.com/forms/d/1tOdLDHozf8vuLNTWHJV5cNVt1vXHFFtunl_Uy7FGFOA'} target="_blank" className='text-white text-sm flex flex-row justify-start'>
-                Hội thảo thứ 7 <Icon.ExternalLink className='w-4 h-4 ml-2' />
-              </Link>
-            </div>
-            <div className='z-10'>
-              {/* The button to open modal */}
-              <label htmlFor="my_modal_6" className=" link text-white text-sm">Hơn nữa</label>
+              <div className='grid grid-cols-2 gap-5'>
+                <Link href={'https://sbay.com.vn/gioi-thieu'} target="_blank" className='text-white text-sm flex flex-row justify-end'>
+                  Giới thiệu về Sbay<Icon.ExternalLink className='w-4 h-4 ml-2' />
+                </Link>
+                <Link href={'https://sbay.com.vn/tin-tuc/chinh-sach-hop-tac-dai-ly-doi-tac/chuong-trinh-hop-tac-dai-ly-ve-may-bay-sbay-viet-nam-n4136'} target="_blank" className='text-white text-sm flex flex-row justify-start'>
+                  Chương trinh hợp tác <Icon.ExternalLink className='w-4 h-4 ml-2' />
+                </Link>
+                <Link href={'https://flight.sbaygroup.com/?dang-nhap=1'} target="_blank" className='text-white text-sm flex flex-row justify-end'>
+                  Trang Thành viên <Icon.ExternalLink className='w-4 h-4 ml-2' />
+                </Link>
+                <Link href={'https://work.sbayvietnam.com'} target="_blank" className='text-white text-sm flex flex-row justify-start'>
+                  Trang WorkPlace <Icon.ExternalLink className='w-4 h-4 ml-2' />
+                </Link>
+                <Link href={'https://meet.google.com/coo-epfp-fnz'} target="_blank" className='text-white text-sm flex flex-row justify-end'>
+                  Meet hàng ngày <Icon.ExternalLink className='w-4 h-4 ml-2' />
+                </Link>
+                <Link href={'https://docs.google.com/forms/d/1tOdLDHozf8vuLNTWHJV5cNVt1vXHFFtunl_Uy7FGFOA'} target="_blank" className='text-white text-sm flex flex-row justify-start'>
+                  Hội thảo thứ 7 <Icon.ExternalLink className='w-4 h-4 ml-2' />
+                </Link>
+              </div>
+              <div className='z-10'>
+                {/* The button to open modal */}
+                <label htmlFor="my_modal_6" className=" link text-white text-sm">Hơn nữa</label>
 
-              {/* Put this part before </body> tag */}
-              <input type="checkbox" id="my_modal_6" className="modal-toggle" />
-              <div className="modal">
-                <div className="modal-box bg-sky-800 opacity-90">
-                  <h2 className='text-xl mb-10 text-white font-black'>Link khác</h2>
-                  <div className='grid grid-cols-2 gap-5 z-10'>
-                    <Link href={'https://drive.google.com/drive/folders/1iqikemTZTth-Lrg1C4yq-7ujmjqwTyUT'} target="_blank" className='text-white text-sm flex flex-row justify-center'>
-                      Kho video <Icon.ExternalLink className='w-4 h-4 ml-2' />
-                    </Link>
-                    <Link href={'https://drive.google.com/drive/folders/18lFUsC76o11mxfAuAQ9PFkZAS0z-a4J9'} target="_blank" className='text-white text-sm flex flex-row justify-center'>
-                      Kho hình ảnh <Icon.ExternalLink className='w-4 h-4 ml-2' />
-                    </Link>
-                    {/* <Link href={'https://docs.google.com/forms/d/1tOdLDHozf8vuLNTWHJV5cNVt1vXHFFtunl_Uy7FGFOA'} className='text-white text-sm flex flex-row justify-center'>
+                {/* Put this part before </body> tag */}
+                <input type="checkbox" id="my_modal_6" className="modal-toggle" />
+                <div className="modal">
+                  <div className="modal-box bg-sky-800 opacity-90">
+                    <h2 className='text-xl mb-10 text-white font-black'>Link khác</h2>
+                    <div className='grid grid-cols-2 gap-5 z-10'>
+                      <Link href={'https://drive.google.com/drive/folders/1iqikemTZTth-Lrg1C4yq-7ujmjqwTyUT'} target="_blank" className='text-white text-sm flex flex-row justify-center'>
+                        Kho video <Icon.ExternalLink className='w-4 h-4 ml-2' />
+                      </Link>
+                      <Link href={'https://drive.google.com/drive/folders/18lFUsC76o11mxfAuAQ9PFkZAS0z-a4J9'} target="_blank" className='text-white text-sm flex flex-row justify-center'>
+                        Kho hình ảnh <Icon.ExternalLink className='w-4 h-4 ml-2' />
+                      </Link>
+                      {/* <Link href={'https://docs.google.com/forms/d/1tOdLDHozf8vuLNTWHJV5cNVt1vXHFFtunl_Uy7FGFOA'} className='text-white text-sm flex flex-row justify-center'>
                     Đăng kí hội thảo <br /> kết nối kinh doanh Sbay<Icon.ExternalLink className='w-4 h-4 ml-2' />
                   </Link> */}
-                  </div>
-                  <div className="modal-action">
-                    <label htmlFor="my_modal_6" className="btn bg-blue-300">Đóng</label>
+                    </div>
+                    <div className="modal-action">
+                      <label htmlFor="my_modal_6" className="btn bg-blue-300">Đóng</label>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
+
           </div>
         </div>
         {/* <Footer></Footer> */}
